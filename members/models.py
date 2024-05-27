f2a4bbed8b2cd28d68d4b4a587e912abc512f4d9
krishnedu <krishnedu@thewholewave.com>
2024-05-27 14:13:42 +0530

    Initial commit with local project files

---
+++ members/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  phone = models.IntegerField(null=True)
+  joined_date = models.DateField(null=True)
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
+
+class Post(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
